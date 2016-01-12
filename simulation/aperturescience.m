f9ebada5f191df0f22944b345ec5a3b963e06fa0
eaglewyng <parker.ridd@gmail.com>
2016-01-12 16:59:17 -0700

    added other team's code

---
+++ simulation/aperturescience.m
@@ -0,0 +1,235 @@
+% main control code - assumes full state knowledge
+%
+%
+% Modified: 
+%   2/11/2014 - R. Beard
+%   2/18/2014 - R. Beard
+%   2/24/2014 - R. Beard
+%
+
+function aperturescience=controller_home_full_state(uu,P)
+
+    % process inputs to function
+    % robots - own team
+    for i=1:P.num_robots,
+        robot(:,i)   = uu(1+3*(i-1):3+3*(i-1));
+    end
+    NN = 3*P.num_robots;
+    % robots - opponent
+    for i=1:P.num_robots,
+        opponent(:,i)   = uu(1+3*(i-1)+NN:3+3*(i-1)+NN);
+    end
+    NN = NN + 3*P.num_robots;
+    % ball
+    ball = [uu(1+NN); uu(2+NN)];
+    NN = NN + 2;
+    % score: own team is score(1), opponent is score(2)
+    score = [uu(1+NN); uu(2+NN)];
+    NN = NN + 2;
+    % current time
+    t      = uu(1+NN);
+    
+    
+    % robot #1 positions itself behind ball and rushes the goal.
+    %v1 = play_rush_goal(robot(:,1), ball, P);
+    v1 = play_bank_shot(robot(:,1), ball, P,t);
+    v2 = play_screen_opponent_from_goal(robot(:,2), opponent(:,2), P);
+%     if ((score(1)+1) < score(2))
+%         v2 = play_rush_goal(robot(:,2), ball, P);
+%     else
+%         v2 = skill_follow_ball_on_line(robot(:,2), ball, -2*P.field_width/3, P);
+%     end
+
+    
+    % output velocity commands to robots
+    v1 = utility_saturate_velocity(v1,P);
+    v2 = utility_no_velocity(v2,P);
+    v_c = [v1; v2];
+end
+
+%-----------------------------------------
+% play - rush goal
+%   - go to position behind ball
+%   - if ball is between robot and goal, go to goal
+% NOTE:  This is a play because it is built on skills, and not control
+% commands.  Skills are built on control commands.  A strategy would employ
+% plays at a lower level.  For example, switching between offense and
+% defense would be a strategy.
+function v = play_rush_goal(robot, ball, P)
+  
+  % normal vector from ball to goal
+  n = P.goal-ball;
+  n = n/norm(n);
+  % compute position 10cm behind ball, but aligned with goal.
+  position = ball - 0.2*n;
+    
+  if norm(position-robot(1:2))<.21,
+      v = skill_go_to_point(robot, P.goal, P);
+  else
+      v = skill_go_to_point(robot, position, P);
+  end
+
+end
+
+function v = play_bank_shot(robot, ball, P,t)
+  
+    persistent play_state
+    
+    if(t<2)
+       play_state=0;
+    end
+    
+
+  % normal vector from ball to goal
+  n = [P.goal(1) ; (P.goal(2)+P.field_width)]-ball;
+  n = n/norm(n);
+  % compute position 10cm behind ball, but aligned with goal.
+  position = ball - 0.2*n;
+  
+  
+  switch play_state
+    case 0
+          if norm(position-robot(1:2))<.1,
+              play_state = 1;
+              v = skill_go_to_point_for_wall_shot(robot, [P.goal(1) ; (P.goal(2)+P.field_width)], P);
+        %       v = skill_go_to_point_for_wall_shot(robot, position, P);
+        
+          else
+              v = skill_go_to_point_for_wall_shot(robot, position, P);
+          end
+    case 1
+      v = skill_go_to_point_for_wall_shot(robot, [P.goal(1) ; (P.goal(2)+P.field_width)]*.4, P);
+    otherwise
+      v = skill_go_to_point_for_wall_shot(robot, position, P);
+   end
+
+  
+%   if norm(position-robot(1:2))<.1,
+%       v = skill_go_to_point_for_wall_shot(robot, [P.goal(1) ; (P.goal(2)+P.field_width)], P);
+% %       v = skill_go_to_point_for_wall_shot(robot, position, P);
+%   else
+%       v = skill_go_to_point_for_wall_shot(robot, position, P);
+%   end
+%   
+  
+
+end
+
+function v = play_screen_opponent_from_ball(robot, opponent, ball, P)
+    n = opponent(1:2)-ball;
+    n = n/norm(n);
+    
+    position = opponent(1:2) - .2*n;
+    v = skill_go_to_point(robot, position,P);
+end
+
+function v = play_screen_opponent_from_goal(robot, opponent, P)
+    n = opponent(1:2)- P.goal;
+    n = n/norm(n);
+    
+    position = opponent(1:2) - .2*n;
+    v = skill_go_to_point(robot, position,P);
+end
+
+function v = play_prepare_for_wall_shot(robot, ball, P)
+  % normal vector from ball to goal
+  n = P.goal-ball;
+  n = n/norm(n);
+  % compute position 10cm behind ball, but aligned with goal.
+  position = ball - 0.2*n;
+    
+  if norm(position-robot(1:2))<.21,
+      v = skill_go_to_point_for_wall_shot(robot, P.goal, P);
+  else
+      v = skill_go_to_point_for_wall_shot(robot, position, P);
+  end
+end
+
+%-----------------------------------------
+% skill - follow ball on line
+%   follows the y-position of the ball, while maintaining x-position at
+%   x_pos.  Angle always faces the goal.
+
+function v=skill_follow_ball_on_line(robot, ball, x_pos, P)
+
+    % control x position to stay on current line
+    vx = -P.control_k_vx*(robot(1)-x_pos);
+    
+    % control y position to match the ball's y-position
+    vy = -P.control_k_vy*(robot(2)-ball(2));
+
+    % control angle to -pi/2
+    theta_d = atan2(P.goal(2)-robot(2), P.goal(1)-robot(1));
+    omega = -P.control_k_phi*(robot(3) - theta_d); 
+    
+    v = [vx; vy; omega];
+end
+
+%-----------------------------------------
+% skill - go to point
+%   follows the y-position of the ball, while maintaining x-position at
+%   x_pos.  Angle always faces the goal.
+
+function v=skill_go_to_point(robot, point, P)
+
+    % control x position to stay on current line
+    vx = -P.control_k_vx*(robot(1)-point(1));
+    
+    % control y position to match the ball's y-position
+    vy = -P.control_k_vy*(robot(2)-point(2));
+
+    % control angle to -pi/2
+    theta_d = atan2(P.goal(2)-robot(2), P.goal(1)-robot(1));
+    omega = -P.control_k_phi*(robot(3) - theta_d); 
+    
+    v = [vx; vy; omega];
+end
+
+function v = skill_go_to_point_for_wall_shot(robot, point, P)
+
+    % control x position to stay on current line
+    vx = -P.control_k_vx*(robot(1)-point(1));
+    
+    % control y position to match the ball's y-position
+    vy = -P.control_k_vy*(robot(2)-point(2));
+
+    % control angle to -pi/2
+    theta_d = atan2((P.goal(2)+P.field_width)-robot(2), P.goal(1)-robot(1));
+    omega = -P.control_k_phi*(robot(3) - theta_d); 
+    
+    v = [vx; vy; omega];
+end
+
+%------------------------------------------
+% utility - saturate_velocity
+% 	saturate the commanded velocity 
+%
+function v = utility_saturate_velocity(v,P)
+    if v(1) >  P.robot_max_vx,    v(1) =  P.robot_max_vx;    end
+    if v(1) < -P.robot_max_vx,    v(1) = -P.robot_max_vx;    end
+    if v(2) >  P.robot_max_vy,    v(2) =  P.robot_max_vy;    end
+    if v(2) < -P.robot_max_vy,    v(2) = -P.robot_max_vy;    end
+    if v(3) >  P.robot_max_omega, v(3) =  P.robot_max_omega; end
+    if v(3) < -P.robot_max_omega, v(3) = -P.robot_max_omega; end
+end
+
+function v = utility_half_velocity(v,P)
+    if v(1) >  P.robot_max_vx/2,    v(1) =  P.robot_max_vx/2;    end
+    if v(1) < -P.robot_max_vx/2,    v(1) = -P.robot_max_vx/2;    end
+    if v(2) >  P.robot_max_vy/2,    v(2) =  P.robot_max_vy/2;    end
+    if v(2) < -P.robot_max_vy/2,    v(2) = -P.robot_max_vy/2;    end
+    if v(3) >  P.robot_max_omega, v(3) =  P.robot_max_omega; end
+    if v(3) < -P.robot_max_omega, v(3) = -P.robot_max_omega; end
+end
+
+function v = utility_no_velocity(v,P)
+    if v(1) >  0,    v(1) =  0;    end
+    if v(1) < 0,    v(1) = 0;    end
+    if v(2) > 0,    v(2) =  0;    end
+    if v(2) < 0,    v(2) = 0;    end
+    if v(3) >  P.robot_max_omega, v(3) =  P.robot_max_omega; end
+    if v(3) < -P.robot_max_omega, v(3) = -P.robot_max_omega; end
+end
+
+
+  

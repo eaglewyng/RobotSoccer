b07c37b43ec41cbe4a5f04bc08e4779c14b8a78a
Connor Smith <connor.smith.256@gmail.com>
2016-01-26 12:03:35 -0700

    started working on using HSV for color identification

--- robot/vision/read_stream.py
+++ robot/vision/read_stream.py
@@ -6,9 +6,9 @@ stream_path = "http://192.168.1.10:8080/stream?topic=/image&dummy=param.mjpg"
 
 # boundary values are in reverse RGB order (BGR)
 boundaries = [
-    ([0  , 0  , 75 ], [100, 100, 255]),              # red
-    ([0  , 75 , 0  ], [120, 255, 120]),              # green
-    ([75 , 0  , 0  ], [255, 255, 50 ]),              # blue
+    # ([0  , 0  , 75 ], [100, 100, 255]),              # red
+    # ([0  , 75 , 0  ], [120, 255, 120]),              # green
+    ([75 , 100  , 100 ], [130, 255, 255 ]),              # blue
 ]
 
 def main():
@@ -26,20 +26,21 @@ def main():
 		if start != -1 and end != -1:
 			jpg = bytes[start:end+2]
 			bytes = bytes[end+2:]
-			image = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.CV_LOAD_IMAGE_COLOR)
+			image_rgb = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.CV_LOAD_IMAGE_COLOR)
+			image_hsv = cv2.cvtColor(image_rgb, cv2.COLOR_BGR2HSV)
 
-			image = cv2.resize(image, (0, 0), fx=0.6, fy=0.6)
 			output = [None for x in boundaries]
 			for i, (lower, upper) in enumerate(boundaries):
 			    lower = np.array(lower, dtype = "uint8")
 			    upper = np.array(upper, dtype = "uint8")
 
-			    mask = cv2.inRange(image, lower, upper)
-			    output[i] = cv2.bitwise_and(image, image, mask = mask)
+			    mask = cv2.inRange(image_hsv, lower, upper)
+			    result = cv2.bitwise_and(image_hsv, image_hsv, mask = mask)
+			    output[i] = cv2.cvtColor(result, cv2.COLOR_HSV2BGR)
 
-			images = [image] + output
+			images = [image_rgb] + output
 			cv2.imshow('images', np.vstack(images))
-			cv2.imwrite("images.jpg", image)
+			cv2.imwrite("images.jpg", image_rgb)
 			if cv2.waitKey(1) == 27:
 				break
 

221db5f66b82efef03c4ac289867ae001d07d11c
Connor Smith <connor.smith.256@gmail.com>
2016-02-02 11:03:16 -0700

    cleaned up catkin vision directory

--- computer/vision/src/robot_soccer/scripts/utilities/battery.py
+++
@@ -1,7 +0,0 @@
-from motor_control import roboclaw
-
-reading = roboclaw.readmainbattery()
-voltage = (1.0 * reading)/10.0
-print "Battery Level: %.1f V" %voltage
-if voltage < 10.0:
-  print "WARNING: Battery low, change soon!"
--- computer/vision/src/robot_soccer/scripts/utilities/kill.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-from motor_control import roboclaw
-roboclaw.M1Forward(128,0)
-roboclaw.M1Forward(129,0)
-roboclaw.M2Forward(128,0)

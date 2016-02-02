f2f829b48e3eac7239a825e29b20ebba0057b4b2
eaglewyng <parker.ridd@byu.net>
2016-02-02 16:48:23 +0000

    added directional scripts

---
+++ robot/motion/backward.py
@@ -0,0 +1,17 @@
+import roboclaw, time
+
+i = 10
+flag = True
+while i != -10:
+  print(i)
+  roboclaw.M2Forward(128,i)
+  roboclaw.M1Backward(128,i)
+  i = i + 10 if flag else i - 10
+  if i > 120:
+    i = 120
+    flag = False
+  if i < 0:
+    i = 0
+    #flag = True
+  time.sleep(0.05)
+
---
+++ robot/motion/right.py
@@ -0,0 +1,18 @@
+import roboclaw, time, math
+
+i = 10
+flag = True
+while i != -10:
+  print(i)
+  roboclaw.M2Forward(128,int(round(i/math.sqrt(3.0))))
+  roboclaw.M1Forward(128,int(round(i/math.sqrt(3.0))))
+  roboclaw.M1Backward(129,i)
+  i = i + 10 if flag else i - 10
+  if i > 120:
+    i = 120
+    flag = False
+  if i < 0:
+    i = 0
+    #flag = True
+  time.sleep(0.05)
+

3a0b564fa32fd47a3bb2844b1181ce17d4d5fe3e
Parker Ridd <pbridd@byu.net>
2016-02-24 10:15:58 -0700

    fixed class definition

--- robot/motion/src/motion/scripts/gamepieces/Ball.py
+++ robot/motion/src/motion/scripts/gamepieces/Ball.py
@@ -1,13 +1,13 @@
 from MotionSkills import *
 
-class BallLocation(self):
+class BallLocation():
   def __init__(self, timestamp = 0, x = 0.0, y = 0.0, magnitude = 0.0, angle = 0.0):
     self.timestamp = timestamp
     self.x = x
     self.y = y
     self.magnitude = magnitude
     self.angle = angle
-    
+
 
 class Ball:
     def __init__(self):

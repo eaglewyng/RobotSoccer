91e24fd8420edbc3e0937df0513d75fca91bd555
Parker Ridd <pbridd@byu.net>
2016-02-25 11:02:00 -0700

    altered computer script to write to log files

--- misc_scripts/computer_setup.sh
+++ misc_scripts/computer_setup.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # start ros
-roscore > /dev/null &
+roscore > /tmp/roscore_log.txt &
 
 # start vision
 cd ~/RobotSoccer/computer/vision
 source devel/setup.bash
-rosrun robot_soccer computer_vision > /dev/null &
+rosrun robot_soccer computer_vision > /tmp/vision_log.txt &
 
 # start command center
 cd ~/RobotSoccer/robot/motion
 source devel/setup.bash
 rosrun robot_soccer VektoryCommandCenter.py
+

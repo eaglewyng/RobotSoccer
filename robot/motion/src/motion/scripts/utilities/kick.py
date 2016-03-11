6f1adcf8de10b34a5b061aa716e1ddb92c624e62
eaglewyng <parker.ridd@byu.net>
2016-03-11 14:21:52 -0700

    Corrected kicking

--- robot/motion/src/motion/scripts/utilities/kick.py
+++ robot/motion/src/motion/scripts/utilities/kick.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 import os
+def initkick():
+  os.system("echo 29 > /sys/class/gpio/export")
+  os.system("echo out > /sys/class/gpio/gpio29/direction")
+  
 def kick():
-  os.system("echo 0 > /sys/class/gpio/gpio204/value; sleep .1; echo 1 > /sys/class/gpio/gpio204/value")
+  os.system("echo 1 > /sys/class/gpio/gpio29/value; sleep .1; echo 0 > /sys/class/gpio/gpio29/value")
 
 if __name__ == '__main__':
   kick()

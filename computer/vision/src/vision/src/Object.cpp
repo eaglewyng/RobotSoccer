cb7af2024230c36fadb1cb02bf98b0a88bad0ff8
Parker Ridd <pbridd@byu.net>
2016-03-15 15:34:06 -0600

    used this keyword properly

--- computer/vision/src/vision/src/Object.cpp
+++ computer/vision/src/vision/src/Object.cpp
@@ -10,14 +10,14 @@
 using namespace cv;
 
 Object::Object() {
-  Object::x_pos = 0;
-  Object::y_pos = 0;
-  Object::img_x = 455;
-  Object::img_y = 240;
-  Object::old_x_pos = 0;
-  Object::old_y_pos = 0;
-  Object::HSVmax = 0;
-  Object::HSVmin = 0;
+  this->x_pos = 0;
+  this->y_pos = 0;
+  this->img_x = 455;
+  this->img_y = 240;
+  this->old_x_pos = 0;
+  this->old_y_pos = 0;
+  this->HSVmax = 0;
+  this->HSVmin = 0;
 }
 
 Object::~Object() {
@@ -26,59 +26,59 @@ Object::~Object() {
 
 // Setters and Getters
 void Object::set_x_pos(int x) {
-  Object::old_x_pos = Object::x_pos; // save old position
-  Object::x_pos = x;
+  this->old_x_pos = this->x_pos; // save old position
+  this->x_pos = x;
 }
 
 int Object::get_x_pos() {
-  return Object::x_pos;
+  return this->x_pos;
 }
 
 void Object::set_img_x(int x) {
-  Object::img_x = x;
+  this->img_x = x;
 }
 
 int Object::get_img_x() {
-  return Object::img_x;
+  return this->img_x;
 }
 
 void Object::set_img_y(int y) {
-  Object::img_y = y;
+  this->img_y = y;
 }
 
 int Object::get_img_y() {
-  return Object::img_y;
+  return this->img_y;
 }
 
 void Object::set_y_pos(int y) {
-  Object::old_y_pos = Object::y_pos; // save old position
-  Object::y_pos = y;
+  this->old_y_pos = this->y_pos; // save old position
+  this->y_pos = y;
 }
 
 int Object::get_y_pos() {
-  return Object::y_pos;
+  return this->y_pos;
 }
 
 int Object::get_old_y() {
-  return Object::old_y_pos;
+  return this->old_y_pos;
 }
 
 int Object::get_old_x() {
-  return Object::old_x_pos;
+  return this->old_x_pos;
 }
 
 cv::Scalar Object::getHSVmin() {
-  return Object::HSVmin;
+  return this->HSVmin;
 }
 
 void Object::setHSVmin(cv::Scalar min) {
-  Object::HSVmin = min;
+  this->HSVmin = min;
 }
 
 cv::Scalar Object::getHSVmax() {
-  return Object::HSVmax;
+  return this->HSVmax;
 }
 
 void Object::setHSVmax(cv::Scalar max) {
-  Object::HSVmax = max;
+  this->HSVmax = max;
 }
